d024cc672f0ff89e7647987a9aa48ef578778e38
面﨑祐一 <Uone@Omozaki-no-MacBook-Air.local>
2018-11-14 23:19:07 +0900

    11/14 23:19

--- eclipse_workspace/Junior_ex3/src/main/Sketch.java
+++ eclipse_workspace/Junior_ex3/src/main/Sketch.java
@@ -5,9 +5,15 @@ import java.io.IOException;
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
+	int x_zero = 50;
+	int y_zero = 50;
+	int x_limit = 500;
+	int y_limit = 500;
+
+	Fuzzy2 f_ex1;
 
 	public void settings() {
-		size(500, 500);
+		size(600, 600);
 	}
 
 	public void setup() {
@@ -16,7 +22,7 @@ public class Sketch extends PApplet {
 		String inputPath2 = "src/kadai3_pattern2.txt";
 
 		try {
-			Fuzzy2 f_ex1 = new Fuzzy2(inputPath1);
+			f_ex1 = new Fuzzy2(inputPath1);
 			drawDataset(f_ex1);
 			int[] ruleFlg = f_ex1.optimisation();
 
@@ -26,25 +32,37 @@ public class Sketch extends PApplet {
 	}
 
 	public void draw() {
+//		drawDataset(f_ex1);
 
 	}
 
 	public void drawDataset(Fuzzy2 f) {
+		stroke(0);
+		line(x_zero, y_zero, x_zero, y_limit + y_zero);
+		line(x_zero, y_limit + y_zero, x_limit + x_zero, y_limit + y_zero);
+		line(x_limit + x_zero, y_limit + y_zero, x_limit + x_zero, y_zero);
+		line(x_limit + x_zero, y_zero, x_zero, y_zero);
 		for (int p = 0; p < f.x.length; p++) {
 			if (f.y[p] == 0) {
 				stroke(0);
 				fill(255, 0, 0);
-				ellipse((float) f.x[p][0] * width, height - (float) f.x[p][1] * height, 10, 10);
+				ellipse((float) f.x[p][0] * x_limit + x_zero,
+						height - ((float) f.x[p][1] * y_limit + y_zero),
+						10, 10);
 			}
 			if (f.y[p] == 1) {
 				stroke(0);
 				fill(0, 255, 0);
-				ellipse((float) f.x[p][0] * width, height - (float) f.x[p][1] * height, 10, 10);
+				ellipse((float) f.x[p][0] * x_limit + x_zero,
+						height - ((float) f.x[p][1] * y_limit + y_zero),
+						10, 10);
 			}
 			if (f.y[p] == 2) {
 				stroke(0);
 				fill(0, 0, 255);
-				ellipse((float) f.x[p][0] * width, height - (float) f.x[p][1] * height, 10, 10);
+				ellipse((float) f.x[p][0] * x_limit + x_zero,
+						height - ((float) f.x[p][1] * y_limit + y_zero),
+						10, 10);
 			}
 		}
 	}
