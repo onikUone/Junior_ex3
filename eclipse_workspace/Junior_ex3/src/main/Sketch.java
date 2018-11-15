3cfb28f1b5588fe2363f41c7fafcfe5d7d61e265
面﨑祐一 <Uone@Omozaki-no-MacBook-Air.local>
2018-11-16 00:41:24 +0900

    axis追加

--- eclipse_workspace/Junior_ex3/src/main/Sketch.java
+++ eclipse_workspace/Junior_ex3/src/main/Sketch.java
@@ -5,16 +5,15 @@ import java.io.IOException;
 import processing.core.PApplet;
 
 public class Sketch extends PApplet {
-	int x_zero = 50;
-	int y_zero = 50;
-	int x_limit = 500;
-	int y_limit = 500;
+	int size = 700;
+	int limit = 500;
+	int zero = (size - limit) / 2;
 
 	Fuzzy f_ex1;
 	Fuzzy2 f2_ex1;
 
 	public void settings() {
-		size(600, 600);
+		size(size, size);
 	}
 
 	public void setup() {
@@ -27,45 +26,95 @@ public class Sketch extends PApplet {
 			f_ex1.readFile(inputPath1);
 			drawDataset(f_ex1);
 
-//			f2_ex1 = new Fuzzy2(inputPath1);
-//			drawDataset(f2_ex1);
-//			int[] ruleFlg = f2_ex1.optimisation();
+			//			f2_ex1 = new Fuzzy2(inputPath1);
+			//			drawDataset(f2_ex1);
+			//			int[] ruleFlg = f2_ex1.optimisation();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
 	public void draw() {
-//		drawDataset(f_ex1);
+		//		drawDataset(f_ex1);
 
 	}
 
-	public void drawDataset(Fuzzy f) {
+	public void drawAxis() {
 		stroke(0);
-		line(x_zero, y_zero, x_zero, y_limit + y_zero);
-		line(x_zero, y_limit + y_zero, x_limit + x_zero, y_limit + y_zero);
-		line(x_limit + x_zero, y_limit + y_zero, x_limit + x_zero, y_zero);
-		line(x_limit + x_zero, y_zero, x_zero, y_zero);
+		line(zero, zero, zero, limit + zero);
+		line(zero, limit + zero, limit + zero, limit + zero);
+		line(limit + zero, limit + zero, limit + zero, zero);
+		line(limit + zero, zero, zero, zero);
+
+		for (int i = 0; i < 10; i++) {
+
+			if (i % 2 == 0) {
+				line(zero + (limit / 10) * i,
+						height - zero,
+						zero + (limit / 10) * i,
+						height - (zero + 15));
+
+				line(zero + (limit / 10) * i,
+						height - (zero + limit),
+						zero + (limit / 10) * i,
+						height - (zero + limit - 15));
+
+				line(zero,
+						zero + (limit / 10) * i,
+						zero + 15,
+						zero + (limit / 10) * i);
+
+				line(zero + limit,
+						zero + (limit / 10) * i,
+						zero + limit - 15,
+						zero + (limit / 10) * i);
+			}
+			else {
+				line(zero + (limit / 10) * i,
+						height - zero,
+						zero + (limit / 10) * i,
+						height - (zero + 5));
+
+				line(zero + (limit / 10) * i,
+						height - (zero + limit),
+						zero + (limit / 10) * i,
+						height - (zero + limit - 5));
+
+				line(zero,
+						zero + (limit / 10) * i,
+						zero + 5,
+						zero + (limit / 10) * i);
+
+				line(zero + limit,
+						zero + (limit / 10) * i,
+						zero + limit - 5,
+						zero + (limit / 10) * i);
+			}
+		}
+	}
+
+	public void drawDataset(Fuzzy f) {
+		drawAxis();
 		for (int p = 0; p < f.x.length; p++) {
 			if (f.classof_x[p] == 0) {
 				stroke(0);
 				fill(255, 0, 0);
-				ellipse((float) f.x[p][0] * x_limit + x_zero,
-						height - ((float) f.x[p][1] * y_limit + y_zero),
+				ellipse((float) f.x[p][0] * limit + zero,
+						height - ((float) f.x[p][1] * limit + zero),
 						10, 10);
 			}
 			if (f.classof_x[p] == 1) {
 				stroke(0);
 				fill(0, 255, 0);
-				ellipse((float) f.x[p][0] * x_limit + x_zero,
-						height - ((float) f.x[p][1] * y_limit + y_zero),
+				ellipse((float) f.x[p][0] * limit + zero,
+						height - ((float) f.x[p][1] * limit + zero),
 						10, 10);
 			}
 			if (f.classof_x[p] == 2) {
 				stroke(0);
 				fill(0, 0, 255);
-				ellipse((float) f.x[p][0] * x_limit + x_zero,
-						height - ((float) f.x[p][1] * y_limit + y_zero),
+				ellipse((float) f.x[p][0] * limit + zero,
+						height - ((float) f.x[p][1] * limit + zero),
 						10, 10);
 			}
 		}
